99eb83a1ac2b5e2927429380961e7b31944a9380
unknown <xrhstos_kol@hotmail.com>
2022-12-03 15:51:38 +0200

    Data Entities

---
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -0,0 +1,40 @@
+package com.luv2code.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+/**
+ * Created by Chris on 03/12/2022.
+ */
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    private Long id ;
+
+    private String sku ;
+
+    private String name ;
+
+    private String description ;
+
+    private BigDecimal unitPrice ;
+
+    private String imageUrl ;
+
+    private boolean active ;
+
+    private int unitsInStock ;
+
+    private Date dateCreated ;
+
+    private Date lastUpdated ;
+
+}
