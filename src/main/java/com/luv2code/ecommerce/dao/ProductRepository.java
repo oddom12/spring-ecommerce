7ced43c62e1395fc6e6c2263c1fa1ba0c7bdce0c
unknown <xrhstos_kol@hotmail.com>
2022-12-17 14:14:52 +0200

    and cross origin

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -2,10 +2,13 @@ package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by Chris on 10/12/2022.
  */
+
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
 
