9267900f7a2334675caa9d68ad3d15914b8243e7
unknown <xrhstos_kol@hotmail.com>
2022-12-17 14:14:52 +0200

    17/12

--- src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -18,7 +17,6 @@ import java.util.Date;
 @Table(name = "product")
 @Data
 public class Product {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
--- src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
+++ src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -3,7 +3,6 @@ package com.luv2code.ecommerce.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import java.util.Set;
 
